046151c5ab5b4c5db79b18311e8791212abf5b46
Frank Diaz <diazfrankp@gmail.com>
2019-03-11 15:07:41 -0400

    Pet class set.

---
+++ Classes/Pet.cs
@@ -0,0 +1,10 @@
+namespace Oop.Classes
+{
+  public class Pet
+  {
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public bool IsAdopted { get; set; }
+  }
+}
