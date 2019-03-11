357bcba0fd26e09d9f3c9f1108f2a95b5f9b57b4
Frank Diaz <diazfrankp@gmail.com>
2019-03-11 14:42:56 -0400

    created coordinate class

--- Program.cs
+++ Program.cs
@@ -4,6 +4,24 @@ namespace oop
 {
   class Program
   {
+
+    // Model real world data inside our code
+
+    // Store points
+    /*
+    Point has:
+        latitude
+        longitude
+        label
+     */
+
+    public class Coordinate
+    {
+      public decimal Latitude { get; set; }
+      public decimal Longitude { get; set; }
+      public string Label { get; set; }
+    }
+
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
