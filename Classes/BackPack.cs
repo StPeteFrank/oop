8090e90ce6495ba25f58cf587bb2a8f9afb42296
Frank Diaz <diazfrankp@gmail.com>
2019-03-11 15:58:05 -0400

    Created backPack constructors.

--- Classes/BackPack.cs
+++ Classes/BackPack.cs
@@ -7,6 +7,13 @@ namespace Oop.Classes
     public string Color { get; set; }
     public string Owner { get; set; }
     public int NumberOfItems { get; set; } = 0;
-    public int MaxOfNumberOfItems { get; set; }
+    public int MaxNumberOfItems { get; set; }
+
+    public BackPack(string color, string owner, int maxItems)
+    {
+      Color = color;
+      Owner = owner;
+      MaxNumberOfItems = maxItems;
+    }
   }
 }
